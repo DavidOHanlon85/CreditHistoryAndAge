c2eb3f98b7ddf5afc69876b64c656ff3ad57a2b7
DavidOHanlon85 <159072612+DavidOHanlon85@users.noreply.github.com>
2024-09-20 13:28:33 +0100

    Add files via upload

---
+++ Exercise1_IfElseAndLogicalOperators_CreditHistoryAndAge.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package slideDeckTasks;
+
+/**
+ * 
+ */
+public class Exercise1_IfElseAndLogicalOperators_CreditHistoryAndAge {
+
+	public static void main(String[] args) {
+
+		int age = 21;
+		boolean goodCreditHistory = true;
+
+		if (age >= 18 && goodCreditHistory == true) {
+			System.out.println("Your appplication had been successful.");
+		} else {
+			System.out.println("Your appplication had been unsuccessful.");
+		}
+
+	}
+
+}
